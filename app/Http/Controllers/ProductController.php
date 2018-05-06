3af5f0209da8eaf3f3148d31c5258a2809ffef9a
Sherlock <sherlock@gmail.com>
2018-05-06 18:06:17 +0630

    Handles the exception

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -92,6 +92,6 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
-        return response([null,Response::HTTP_NO_CONTENT]);
+        return response('null',Response::HTTP_NO_CONTENT);
     }
 }
